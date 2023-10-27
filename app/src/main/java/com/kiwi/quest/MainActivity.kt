3a6903c921ad35f012db64710d087bf39ffb626c
jonathanbcsouza <jonathanbcsouza@gmail.com>
2023-10-28 02:41:29 +1300

    feat: add text-to-speech

--- app/src/main/java/com/kiwi/quest/MainActivity.kt
+++ app/src/main/java/com/kiwi/quest/MainActivity.kt
@@ -2,7 +2,9 @@ package com.kiwi.quest
 
 import DataLoader
 import Slangs
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
@@ -13,12 +15,16 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import java.util.Locale
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    private lateinit var textToSpeech: TextToSpeech
 
     private lateinit var getOther: Slangs
     private lateinit var btnChangeMsg: Button
+    private lateinit var btnSpeak: Button
     private lateinit var randomMsg: TextView
     private lateinit var backgroundColorAnimation: ConstraintLayout
     private val colorsBackground = ColorsBackground(this)
@@ -26,7 +32,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.v(TAG, "Activity created!")
+
+        textToSpeech = TextToSpeech(this, this)
 
         FirebaseApp.initializeApp(this)
         val databaseReference = FirebaseDatabase.getInstance().reference
@@ -39,14 +46,14 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
-                Log.w(TAG, "Failed to read data.", databaseError.toException())
+                Log.w("MainActivity", "Failed to read data.", databaseError.toException())
             }
         })
 
         getOther = Slangs(listOf("" to ""))
 
         randomMsg = findViewById(R.id.msg)
-        btnChangeMsg = findViewById(R.id.button)
+        btnChangeMsg = findViewById(R.id.btnCreateNew)
         backgroundColorAnimation = findViewById(R.id.background)
 
         btnChangeMsg.setOnClickListener {
@@ -57,9 +64,38 @@ class MainActivity : AppCompatActivity() {
             backgroundColorAnimation.setBackgroundColor(color)
             btnChangeMsg.setTextColor(color)
         }
+
+        btnSpeak = findViewById(R.id.btnSpeak)
+        btnSpeak.setOnClickListener {
+            val textToSpeak = randomMsg.text.toString()
+            if (textToSpeak.isNotEmpty()) {
+                textToSpeech.speak(textToSpeak, TextToSpeech.QUEUE_FLUSH, null, null)
+            }
+        }
+
     }
 
-    companion object {
-        const val TAG = "MainActivity"
+    override fun onInit(status: Int) {
+
+        if (status == TextToSpeech.SUCCESS) {
+            val result = textToSpeech.setLanguage(Locale.US)
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e(TAG, "Language is not available or supported")
+            } else {
+                // Language is supported
+
+            }
+        } else {
+            Log.e(TAG, "Initialization failed")
+        }
     }
+
+    override fun onDestroy() {
+        if (textToSpeech != null) {
+            textToSpeech.stop()
+            textToSpeech.shutdown()
+        }
+        super.onDestroy()
+    }
+
 }
